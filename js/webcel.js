012643e64bd5c69c73550ae23bc6f02f6215db86
anchi84 <anarili@gmail.com>
2017-12-15 18:16:16 +0100

    sort done

--- js/webcel.js
+++ js/webcel.js
@@ -1,19 +1,17 @@
-var names = ["Sanja Simic", "Mihael Jackson", "Sanja Simic", "Suzana Kostic", "Jo Smith", "Tanja Peric", "Dusan Jovanovic", "Petar Petrovic", "Jozef Antic", "Ivana Mihic"];
+var names = ["Sanja Simic", "Mihael Jackson", "Sofija Simic", "Suzana Kostic", "Jo Smith", "Tanja Peric", "Dusan Jovanovic", "Petar Petrovic", "Jozef Antic", "Ivana Mihic"];
 var ages = [50, 34, 30, 24, 50, 28, 56, 14, 53, 45];
 
 function avg(){
-	main();
 	var sum = 0;
 	for(var i=0; i<ages.length; i++){
 		sum = sum + ages[i];
 	}
 	
 	var el = document.getElementById("avg");
-	el.innerHTML = "Prosek godina je " +sum/ages.length +".";
+	el.innerHTML = "Prosek godina je " +(sum/ages.length).toFixed(1) +"!";
 }
 
 function mini(){
-	main();
 	var min = ages[0];
 	var person;
 	for(var i=1; i<ages.length; i++){
@@ -27,7 +25,6 @@ function mini(){
 }
 
 function maxi(){
-	main();
 	var max = ages[0];
 	var person;
 	for(var i=1; i<ages.length; i++){
@@ -40,40 +37,73 @@ function maxi(){
 	el.innerHTML = "Najvise godina " +max +" ima " +person +".";
 }
 
-function sort(){
-	
+function sortTable(col) {
+  	var table, rows, switching, i, x, y, shouldSwitch;
+  	table = document.getElementById("tbl");
+  	switching = true;
+  	/* Make a loop that will continue until no switching has been done: */
+  	while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare, one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[col];
+      y = rows[i + 1].getElementsByTagName("TD")[col];
+      // Check if the two rows should switch place:
+      
+      if (isNaN(x.innerHTML)) {
+      	if(x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        	// If so, mark as a switch and break the loop:
+        	shouldSwitch = true;
+       		break;
+      	}
+      } else {
+      		if (Number(x.innerHTML) > Number(y.innerHTML)) {
+        		// If so, mark as a switch and break the loop:
+        		shouldSwitch = true;
+        		break;
+      		}
+    	}
+    }
+
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
 }
 
 function add() {
 	var newName = document.getElementById("name").value;
 	var newAge = Number(document.getElementById("age").value);
-	//debugger;
 	names.push(newName);
 	ages.push(newAge);
 	main();
+	document.getElementById("avg").innerHTML="";
+	document.getElementById("min").innerHTML="";
+	document.getElementById("max").innerHTML="";
 }
 
 
-function main() { // function definition
-    /*var exampleArray = [34, 12, 3, 2, 78]; // define array literal
-    for(var arrayIndex = 0; arrayIndex < exampleArray.length; arrayIndex = arrayIndex + 1){ // iterate over array; notice that index of the first element in the array is zero
-        var arrayElement = exampleArray[arrayIndex]; // get array element at the position of the current index
-        console.log(arrayElement); // print array element value to the console
-    }*/
-	
-var index = 1;
-var rows = "<tr><th>ID</th><th>Name</th><th>Age</th></tr> ";
-for(var i=0; i<names.length; i++) {
-	var name = names[i];
-	var age = ages[i];
-	var row = "<tr><td>" +index +"</td><td>" +name +"</td><td>" +age +"</td></tr>";
-	rows = rows + row;
-	index++;
-	
-}
-	
-var el = document.getElementById("tbl");
-el.innerHTML = rows;
+function main() {	
+	var index = 1;
+	var rows = "<tr><th>ID</th><th>Name</th><th>Age</th></tr> ";
 
+	for(var i=0; i<names.length; i++) {
+		var name = names[i];
+		var age = ages[i];
+		var row = "<tr><td>" +index +"</td><td>" +name +"</td><td>" +age +"</td></tr>";
+		rows = rows + row;
+		index++;	
+	}
+
+	var el = document.getElementById("tbl");
+	el.innerHTML = rows;
 }
+
 main(); // function call
